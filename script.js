3e007a4380f8576cd846c9418892c2825e7b5fff
Ash Dan <Arvolley01@gmail.com>
2022-06-19 19:48:10 -0400

    added data.txt and connected it to index.html

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "this text brought to you by Javascript";
 document.querySelector("body").appendChild(h2);
 
-const leftbox = document.createTextNode("Left");
-document.getElementById("left-box").append(leftbox);
 
-leftbox.textContent = "test";
+const list3 = document.createElement('p');
+
