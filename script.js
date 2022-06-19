2e0eb78201925ec591555ca416a5447bf0ab31d4
Ash Dan <Arvolley01@gmail.com>
2022-06-19 19:33:05 -0400

    added grid and some corresponding css

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "this text brought to you by Javascript";
 document.querySelector("body").appendChild(h2);
+
+const leftbox = document.createTextNode("Left");
+document.getElementById("left-box").append(leftbox);
+
+leftbox.textContent = "test";
