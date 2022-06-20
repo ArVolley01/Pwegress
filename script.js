797f02dd9adac6bd58c67832aef35934f6877148
Ash Dan <Arvolley01@gmail.com>
2022-06-19 22:09:26 -0400

    finished website (for now)

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const h2 = document.createElement("h2");
 h2.textContent = "This text brought to you by Javascript";
 document.querySelector("body").appendChild(h2);
 
+let linenum = 0;
+
 function addListeners () {
     function postMessage() {
         const message = document.getElementById("input");
 
+        if (linenum >= 8) {
+            document.getElementById("messages").firstChild.remove();
+        } else {
+            linenum++;
+        }
+
         const line = document.createElement("p");
         line.textContent = message.value;
         
-        document.getElementById("g3").append(line);
+        document.getElementById("messages").append(line);
     }
     
     const button = document.getElementById("submit button");
