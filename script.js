ca712b15d504bf50c295ce29587230e17f0475da
Ash Dan <Arvolley01@gmail.com>
2022-06-19 20:45:00 -0400

    attempted adding functionality, still WIP

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+const fs = require('fs');
+
 const h2 = document.createElement("h2");
 h2.textContent = "this text brought to you by Javascript";
 document.querySelector("body").appendChild(h2);
 
+function addListeners () {
+    function postMessage() {
 
+        alert("here");
+    
+        const inputform = document.getElementById("input");
+    
+        const message = inputform.elements[0];
+    
+        fs.appendFile("./Data.txt", message.textContent);
+    }
+    
+    const button = document.getElementById("submit button");
+    
+    button.addEventListener('click', postMessage);
+}
 
+addListeners();
 
