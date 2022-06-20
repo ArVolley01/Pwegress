89af0d2b3a7a4aa843a9fd10e72ad987babd88cd
Ash Dan <Arvolley01@gmail.com>
2022-06-19 21:26:18 -0400

    added some functionality, but data.txt is a no go

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const fs = require('fs');
 
 const h2 = document.createElement("h2");
-h2.textContent = "this text brought to you by Javascript";
+h2.textContent = "This text brought to you by Javascript";
 document.querySelector("body").appendChild(h2);
 
 function addListeners () {
     function postMessage() {
+        message = document.getElementById("input");
 
-        alert("here");
-    
-        const inputform = document.getElementById("input");
-    
-        const message = inputform.elements[0];
-    
-        fs.appendFile("./Data.txt", message.textContent);
+        document.getElementById("data").append(message.value + "\n");
     }
     
     const button = document.getElementById("submit button");
